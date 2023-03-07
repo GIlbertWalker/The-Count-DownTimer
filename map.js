1da34ab793b0cf9fa023f771aa96fb47ee2ee69e
JUSTICE KOFI ANAN KUMAH <jkakumah1999@gmail.com>
2023-03-07 19:37:49 +0000

    timestamp for timer app

---
+++ map.js
@@ -0,0 +1,7 @@
+let firstArr = [1,2,3,4,5]
+let mappedResults = firstArr.map(x =>x*2)
+
+console.log(mappedResults)
+
+// const now = Date.now()
+// console.log(Date)
---
+++ timestamp.js
@@ -0,0 +1,9 @@
+//timestamps
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+  
