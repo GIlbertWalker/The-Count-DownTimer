e60ab37aff7333f0e2c4d75de6f537f3f11c2f8f
Philip <philipessel2006@gmail.com>
2023-03-07 19:36:00 -0800

    first commit

---
+++ displayTimeLeft.js
@@ -0,0 +1,8 @@
+// function to display timer left
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+}
